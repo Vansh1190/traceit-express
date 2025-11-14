550cdfc5bf3762c77363af5275385385bb78bf5a
vansh <vanshk605@gmail.com>
2025-11-14 18:11:17 +0530

    added mysql support

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const createRequestLogger = require("./middleware/logitRequest");
 const createErrorLogger = require("./middleware/logitError");
 const mongoStorage = require("./storage/mongoStorage");
+const sqlStorage = require("./storage/sqlStorage");
 
 
 
 module.exports = {
   requestLogger: createRequestLogger,
   errorLogger: createErrorLogger,
-  mongoStorage
+  mongoStorage,
+  sqlStorage,
 };
